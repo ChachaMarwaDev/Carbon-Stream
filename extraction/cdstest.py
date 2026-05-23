aa1853ebdd7502768de79118e1faa7a659946de3
Chacha Marwa <chachamarwa003@gmail.com>
2026-05-23 05:40:31 +0300

    update fetch of monthly data

--- extraction/cdstest.py
+++ extraction/cdstest.py
@@ -1,45 +1,18 @@
 import cdsapi
 
-dataset = "reanalysis-era5-single-levels"
-request = {
-    "product_type": ["reanalysis"],
-    "variable": [
-        "10m_u_component_of_wind",
-        "10m_v_component_of_wind",
-        "2m_temperature",
-        "total_precipitation",
-        "surface_solar_radiation_downwards",
-        "snowfall"
-    ],
-    "year": ["1940"],
-    "month": ["01"],
-    "day": [
-        "01", "02", "03",
-        "04", "05", "06",
-        "07", "08", "09",
-        "10", "11", "12",
-        "13", "14", "15",
-        "16", "17", "18",
-        "19", "20", "21",
-        "22", "23", "24",
-        "25", "26", "27",
-        "28", "29", "30",
-        "31"
-    ],
-    "time": [
-        "00:00", "01:00", "02:00",
-        "03:00", "04:00", "05:00",
-        "06:00", "07:00", "08:00",
-        "09:00", "10:00", "11:00",
-        "12:00", "13:00", "14:00",
-        "15:00", "16:00", "17:00",
-        "18:00", "19:00", "20:00",
-        "21:00", "22:00", "23:00"
-    ],
-    "data_format": "netcdf",
-    "download_format": "zip"
-}
-
 client = cdsapi.Client()
-client.retrieve(dataset, request).download()
 
+dataset = "reanalysis-era5-single-levels-monthly-means"
+request = {
+    "product_type": ["monthly_averaged_reanalysis"],
+    "variable": ["2m_temperature", "total_precipitation"],
+    "year": ["2020", "2021", "2022", "2023"],
+    "month": ["01","02","03","04","05","06",
+               "07","08","09","10","11","12"],
+    "time": ["00:00"],
+    "data_format": "netcdf",
+    "area": [36, 68, 8, 98],
+}
+target = "in_era5_monthly.nc"
+
+client.retrieve(dataset, request, target)
